4b47545ab19fbaebdf4af9d1350e1468a41f5d95
Evan S <evan.sapienza@gmail.com>
2013-10-28 12:24:37 -0400

    Initial Commit
    WPF Mockup of the UI, intial push.

---
+++ Snowflake_UI_Mockup/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Snowflake_UI_Mockup
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_Snowflake_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Either load a menu or maybe a website?");
+        }
+
+        private void Image_KinectForWindows_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.microsoft.com/en-us/kinectforwindows/");
+        }
+
+        private void Button_Browse_Click(object sender, RoutedEventArgs e)
+        {
+            Window_Browse browseWindow = new Window_Browse();
+            browseWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Scan_Click(object sender, RoutedEventArgs e)
+        {
+            WindowScan scanWindow = new WindowScan();
+            scanWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Print_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Probably open the Metro App for printing to make use of the charms bar");
+        }
+    }
+}
---
+++ Snowflake_UI_Mockup/Window_Browse.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Snowflake_UI_Mockup
+{
+    /// <summary>
+    /// Interaction logic for Window_Browse.xaml
+    /// </summary>
+    public partial class Window_Browse : Window
+    {
+        public Window_Browse()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(@"Download selected STL files.");
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show(@"Most likely redirect to another page with additional
+                              information, pictures, a more detailed description etc.
+                               Could be cool to have a 3D preview.");
+        }
+    }
+}
